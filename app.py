6154cdfa04258796f620127dcbc7add358031ca7
David Sanders <dsanderscanada@gmail.com>
2016-11-22 21:14:57 -0500

    Removed Authentication object to simplify code.

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 import os
 import logging
 from flask import Flask, request, render_template, current_app, url_for
-from helper.authentication import Authentication
+from helper.authentication import get_azure_signon_uri
 from helper.configuration import Configuration
 from datetime import datetime
-
 
 # Configure logging to a basic level. We will allow logging to be configured
 # later if there is a setting in the config_dict file.
@@ -23,16 +22,27 @@ try:
 
     logging.debug('Loading configuration from the file: {0}'.format(configuration_file))
     configuration.configure_from_file(configuration_file, append=True)
+    configuration.configure_from_dict(
+        config_dictionary={
+            "resource": "https://outlook.office365.com/",
+            "authority": "https://login.microsoftonline.com"
+        },
+        append=True
+    )
 
     required_items_list = ['client_id']
     logging.debug('Validating configuration contains non-empty items for: {0}'.format(required_items_list))
     configuration.validate(required_items=required_items_list)
 except KeyError as ke:
-    logging.error('The environment variable "config" has not been set and IS required.')
+    logging.error(
+        'The environment variable "config" has not been set and must be set. The variable should ' +
+        'point to a json file defining a dictionary of keys and values.'
+    )
     exit(1)
 except FileNotFoundError as fnf:
     logging.error(
-        'The file {0} cannot be found and has been set.'.format(fnf)
+        'The file "{0}" specified in the config environment variable cannot be found.'
+        .format(configuration_file)
     )
     exit(1)
 logging.debug('Configuration complete.')
@@ -40,34 +50,15 @@ logging.debug('Configuration complete.')
 
 @app.route('/', methods=['GET'])
 def index_html():
-    auth = Authentication(redirect_uri=url_for('authenticated', _external=True))
-
-    try:
-        auth.setup(configuration.config_dict.get('client_id', None))
-    except KeyError as ke:
-        logging.error(
-            'The key {0} is not defined as an environment variable and IS required.'.format(ke)
-        )
-        exit(1)
-    except Exception as e:
-        raise
-        exit(1)
-
-    logging.debug('')
-    logging.debug('** WARNING ** WARNING ** WARNING **')
-    logging.debug('In production, the log entries in this block should NEVER be written.')
-    logging.debug('They present sensitive information which WOULD be bad in the wrong hands!')
-    logging.debug('Auth helper  : {0}'.format(auth.dump))
-    logging.debug('Sign on at   : {0}'.format(auth.signon_url))
-    logging.debug('Redirect URI : {0}'.format(auth.redirect_uri))
-    logging.debug('Configuration: {0}'.format(configuration.obfuscated_config))
-    logging.debug('** WARNING ** WARNING ** WARNING **')
-    logging.debug('')
-    logging.debug('In index_html')
-    logging.debug('Method: GET')
+    logging.debug('index_html has been loaded by a client.')
     return render_template(
         'index.html',
-        auth_source=auth.signon_url,
+        auth_source=get_azure_signon_uri(
+            client_id=configuration.config_dict.get('client_id'),
+            resource=configuration.config_dict.get('resource'),
+            redirect_uri=url_for('authenticated', _external=True),
+            authority=configuration.config_dict.get('authority')
+        ),
         publication_date='{0}'.format(datetime.now().strftime('%A %d %b %Y')),
         method=request.method
     )
@@ -90,7 +81,9 @@ def cert_key():
         'cert-key.html'
     )
 
+
 port = configuration.config_dict.get('port', 8000)
 server = configuration.config_dict.get('server', "127.0.0.1")
 
+logging.debug('Starting application on {0}:{1}'.format(server, port))
 app.run(host=server, port=port)
--- helper/authentication.py
+++ helper/authentication.py
@@ -4,127 +4,37 @@ import logging
 from urllib.parse import quote, urlencode
 
 
-class Authentication(object):
-    """
-    An authentication helper for authenticating an application against Microsoft Office 365
-    """
-    _certificate = {
-        "client_id": None,
-        "cert_path": None,
-        "cert_thumb": None
-    }
+def get_azure_signon_uri(
+    client_id=None,
+    resource='https://outlook.office365.com/',
+    redirect_uri=None,
+    authority='https://login.microsoftonline.com'
+):
+    logging.debug('Creating signon URL for request.')
 
-#    _client_id = None
-#    _cert_path = None
-#    _cert_thumb = None
-    _return_uri = {
-        "protocol": "http",
-        "server": "localhost",
-        "port": 8000,
-        "path": "authenticated"
-    }
+    if client_id is None:
+        raise ValueError(
+            'A client id (for the application) must be registered in Azure AD and provided to signon_url'
+        )
 
-    _authority = 'https://login.microsoftonline.com'
-    _redirect_uri = None
-    _resource = 'https://outlook.office365.com/'
+    if redirect_uri is None:
+        raise ValueError('A redirection URI must be provided for after successful authentication')
 
-    def __init__(self, redirect_uri):
-        self._redirect_uri = redirect_uri
+    nonce = str(uuid.uuid4())
 
-    def setup_from_env(self):
-        """Instruct the helper to look for its configuration within environment variables. The
-        Authentication helper will look for client_id, cert_path, and cert_thumb environment
-        variables."""
-        try:
-            logging.debug('Setup with environment variables')
-            self._certificate['client_id'] = os.environ["client_id"]
-            self._certificate['cert_path'] = os.environ["cert_path"]
-            self._certificate['cert_thumb'] = os.environ["cert_thumb"]
-        except KeyError as ke:
-            raise
+    authorize_url = '{0}{1}'.format(authority, '/common/oauth2/authorize?{0}')
 
-    def setup(
-            self,
-            client_id
-    ):
-        """Pass the required variables (client_id, cert_path, and cert_thumb) to the helper
-        instead of reading them from environment variables."""
+    params = \
+        {
+            'client_id': client_id,
+            'redirect_uri': redirect_uri,
+            'response_type': 'code id_token',
+            'scope': 'openid',
+            'nonce': nonce,
+            'prompt': 'admin_consent',
+            'response_mode': 'form_post',
+            'resource': resource,
+        }
 
-        logging.debug('Setup with passed parameter(s)')
-        self._certificate['client_id'] = client_id
-        #self._certificate['cert_path'] = cert_path
-        #self._certificate['cert_thumb'] = cert_thumb
-
-
-    @property
-    def client_id(self):
-        """The client of the application as registered/configured in the Active Directory
-        associated with the Office 365 tenant."""
-        return self._certificate['client_id']
-
-    @property
-    def cert_path(self):
-        """The path (/path/to/certs/filename.cert) to the certificate (including the name
-        of the certificate)."""
-        return self._certificate['cert_path']
-
-    @property
-    def cert_thumb(self):
-        """The thumbprint of the certificate."""
-        return self._certificate['cert_thumb']
-
-    @property
-    def dump(self):
-        """Produce a dictionary containing the client id, cert. path, and cert. thumbprint."""
-        return self._certificate
-
-    @property
-    def authority(self):
-        return self._authority
-
-    @authority.setter
-    def authority(self, value):
-        self._authority = value
-
-    @property
-    def redirect_uri(self):
-        return self._redirect_uri
-
-    @redirect_uri.setter
-    def redirect_uri(self, value):
-        self._redirect_uri = value
-
-    @property
-    def resource(self):
-        return self._resource
-
-    @resource.setter
-    def resource(self, value):
-        self._resource = value
-
-    @property
-    def signon_url(self):
-        if self._certificate['client_id'] is None:
-            raise ValueError(
-                'The client id, certificate path, and certificate thumbprint must be set.'
-            )
-        nonce = str(uuid.uuid4())
-
-        authorize_url = '{0}{1}'.format(self._authority, '/common/oauth2/authorize?{0}')
-        #token_url = '{0}{1}'.format(self._authority, '/{0}/oauth2/token')
-
-        params = \
-            {
-                'client_id': self._certificate['client_id'],
-                'redirect_uri': self._redirect_uri,
-                'response_type': 'code id_token',
-                'scope': 'openid',
-                'nonce': nonce,
-                'prompt': 'admin_consent',
-                'response_mode': 'form_post',
-                'resource': self._resource,
-            }
-
-        authorization_url = authorize_url.format(urlencode(params))
-
-        return authorization_url
+    authorization_url = authorize_url.format(urlencode(params))
+    return authorization_url
