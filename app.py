f20911c73a5c20b6e95c2c5db637c63d192b64a6
David Sanders <dsanderscanada@gmail.com>
2016-11-23 20:01:03 -0500

    Add sample json file and update app.py documentation

--- app.py
+++ app.py
@@ -18,26 +18,39 @@ app.config.from_object(__name__)
 logging.debug('Instantiating a configuration object')
 configuration = Configuration()
 try:
+    # Get the name of the configuration file from an environment variable. A
+    # KeyError will be thrown if this has not been defined.
     configuration_file = os.environ['o365_demo_config']
 
+    # Read the configuration file
     logging.debug('Loading configuration from the file: {0}'.format(configuration_file))
     configuration.configure_from_file(configuration_file, append=True)
 
+    # Define a list of required items that must be defined and non-empty
+    # in the file.
     required_items_list = ['client_id', 'resource', 'authority']
+
+    # Validate the configuration file against the required items list
     logging.debug('Validating configuration contains non-empty items for: {0}'.format(required_items_list))
     configuration.validate(required_items=required_items_list)
 except KeyError as ke:
+    # Key Error means that the environment variable isn't defined.
     logging.error(
-        'The environment variable "config" has not been set and must be set. The variable should ' +
-        'point to a json file defining a dictionary of keys and values.'
+        'The environment variable "o365_demo_config" has not been set ' +
+        'and must be set. The variable should point to a json file ' +
+        'defining a dictionary of keys and values. A sample is' +
+        'provided in the repository.'
     )
     exit(1)
 except FileNotFoundError as fnf:
+    # File Not Found error means the file couldn't be located.
     logging.error(
         'The file "{0}" specified in the config environment variable cannot be found.'
         .format(configuration_file)
     )
     exit(1)
+
+# Configuration is complete
 logging.debug('Configuration complete.')
 
 
